dddc99836c841fe22068e5b3a711c9315b331813
swathi <swathiashok94@gmail.com>
2022-12-06 18:00:23 +0530

    first commit

--- pages/404.js
+++ pages/404.js
@@ -1,17 +1,40 @@
 
-import Link from 'next/link';
+import { useEffect } from 'react';
+import {  useRouter } from 'next/router'
+  const Error = () => {
+  const router = useRouter();
+  const handleerror = () =>{
+    router.push("/");
+  };
+  useEffect( () => {
+    setTimeout( () =>{
+      router.push("/");
+    },3000)
+  },[])
+    return (
+      <>
+    <h1 className='content'>Error 404 Found</h1>
+    <a onClick={ handleerror } className='content'>Back To Home</a>
+    </>
+    )
+  }
+export default Error;
+
+
+
+{/* ***************Method 1************* */}
+/* import Link from 'next/link';
 const Error = () => {
     return (
       <>
     <h1 className='content'>Error 404 Found</h1>
-    {/* ***************Method 1************* */}
     <Link href="/"> 
     <h3 className='content'>Back To Home</h3>
     </Link>
     </>
     )
   }
-export default Error;
+export default Error; */
 
 {/* ***************Method 2************* */}
 /* import {  useRouter } from 'next/router'
